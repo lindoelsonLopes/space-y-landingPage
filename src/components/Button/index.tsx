337628dca520216d4b2b6110724ae5681089a31c
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-08-04 19:00:15 -0300

    Draft v1.0

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { ButtonComponent } from './styles';
+
+interface ButtonProps {
+  text: string;
+}
+
+function Button({ text }: ButtonProps) {
+  return (
+  <>
+    
+    <ButtonComponent>
+      { text }
+    </ButtonComponent>
+  </>  
+
+  );
+}
+
+export default Button;
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,70 @@
+import { createGlobalStyle } from "styled-components";
+
+function pixelToRem(...values: number[]) {
+  return values
+    .reduce((acc, current) => (acc += current / 16 +`rem `), "")
+    .trim();
+}
+
+export const GlobalStyles = createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+html {
+  @media(max-width: 1440px) {
+    // font-size: 93.75%;
+  }
+  @media(max-width: 375px) {
+    // font-size: 87.5%;
+  }
+}
+
+body {
+    font-family: 'Heebo', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+:root {
+  --space: #0B1E8A;  
+  --space-light: #3D68B2;
+  --space-dark: #040327;
+  --mars: #E85937;
+  --mars-light: #FF8C70;
+  --mars-dark: #CF3F1D;
+  --gray-01: #0D0E13;
+  --gray-02: #2C2D3A;
+  --gray-03: #60616F;
+  --gray-04: #898A93;
+  --gray-05: #D5D5DB;
+  --uranus: #24FFE9;
+  --sun: #F5D15F;
+  --jupiter: #E00E34;
+  --earth: #0FDD86;
+  --saturn: #554AD7;
+  --text: #FFFFFF;
+
+--font-display: 800 ${pixelToRem(62)}/ ${pixelToRem(96)} "Heebo", sans-serif;
+
+--font-heading-1: 700 ${pixelToRem(32)}/ ${pixelToRem(48)} "Heebo", sans-serif;
+
+--font-heading-2: 500 ${pixelToRem(24)}/ ${pixelToRem(32)} "Heebo", sans-serif;
+
+--font-heading-3: 400 ${pixelToRem(20)}/ ${pixelToRem(24)} "Heebo", sans-serif;
+
+--text-4: 500 ${pixelToRem(18)}/ ${pixelToRem(48)} "Heebo", sans-serif;
+
+--text-3: 500 ${pixelToRem(18)}/ ${pixelToRem(24)} "Heebo", sans-serif;
+
+--text-2: 500 ${pixelToRem(14)}/ ${pixelToRem(20)} "Heebo", sans-serif;
+
+--text-1: 400 ${pixelToRem(16)}/ ${pixelToRem(24)} "Heebo", sans-serif;
+
+--text-0: 400 ${pixelToRem(14)}/ ${pixelToRem(24)} "Heebo", sans-serif;
+
+}
+`
